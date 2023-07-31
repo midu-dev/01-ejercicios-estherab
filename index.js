508e446889772a17aa4159f0296af0f74f6969e7
estherab <esther.abenito@gmail.com>
2023-07-31 14:20:31 +0200

    Ejercicio 3

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Ejercicio 2
 const fs = require('node:fs/promises')
-const path = require('node:path');
+const path = require('node:path')
 
 async function writeFile (filePath, data, callback) {
-  const directory = path.dirname(filePath);
+  const directory = path.dirname(filePath)
 
   await fs.mkdir(directory).then(() =>  {
     console.log('Directorio creado')
@@ -17,12 +17,28 @@ async function writeFile (filePath, data, callback) {
     console.log('Error al escribir en el archivo')
   });
 
-  callback();
+  callback()
 }
 
 // Ejercicio 3
 async function readFileAndCount (word, callback) {
+  const file = process.argv[2]
+  let times = 0;
 
+  if (!file)
+    return callback(new Error('No se ha especificado el path del archivo'), 0)
+
+  if (!word)
+    return callback(new Error('No se ha especificado la palabra a buscar'), 0)
+
+  try {
+    const content = await fs.readFile(file, 'utf-8')
+    times = content.split(' ').filter(item => item.includes(word)).length
+    
+    return callback(null, times)
+  } catch (error) {
+    return callback(null, 0)
+  }
 }
 
 module.exports = {
