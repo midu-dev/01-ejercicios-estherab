f7a188f51b574c130bfe91f0aa1c780a678a8d94
estherab <esther.abenito@gmail.com>
2023-07-31 12:59:27 +0200

    Ejercicios 1 y 2 final

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
 // Ejercicio 2
-/* path: el path del archivo a escribir
-content: el contenido del archivo a escribir
-callback: una función que se ejecutará cuando se haya escrito el archivo
-La función debe escribir en el archivo especificado en el path el contenido especificado en el parámetro content. 
-Cuando termine de escribir el archivo, debe ejecutar la función callback que recibe como parámetro.
-
-Si el archivo ya existe, debe ser reemplazado. Si el path no existe, debe ser creado.
-
-Pistas: fs.mkdir puede crear directorios que no existen. Revisa la documentación de Node.js para ver cómo 
-se usa y sus posibles opciones. */
 const fs = require('node:fs/promises')
+const path = require('node:path');
 
 async function writeFile (filePath, data, callback) {
+  const directory = path.dirname(filePath);
 
-  try {
-    await fs.mkdir(filePath, { recursive: true })
-  } catch (error) {
-    console.log("Error al crear el directorio");
-  }
+  await fs.mkdir(directory).then(() =>  {
+    console.log('Directorio creado')
+  }).catch(() => {
+    console.log('Error al crear el directorio')
+  });
 
-  try {
-    await fs.writeFile(filePath, data)
-  } catch (error) {
-    console.log("Error al escribir en el archivo");
-  }
+  await fs.writeFile(filePath, data).then(() => {
+    console.log('Escritura en archivo correcta')
+  }).catch(() => {
+    console.log('Error al escribir en el archivo')
+  });
 
   callback();
 }
